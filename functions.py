46ff1ef82d048161513c3fd593238678f9d4ad0a
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2025-08-14 12:00:48 +0200

    update

--- functions.py
+++ functions.py
@@ -13,6 +13,12 @@ import os
 join = os.path.join
 import SimpleITK as sitk
 from typing import List
+from multiprocessing import Pool
+from batchgenerators.utilities.file_and_folder_operations import *
+import nibabel as nib
+from nibabel import io_orientation
+import numpy as np
+import shutil
 
 def maybe_mkdir_p(directory: str) -> None:
     os.makedirs(directory, exist_ok=True)
@@ -69,8 +75,13 @@ def load_DICOM_data_SITK(path_to_series):
     img_data = sitk.GetArrayFromImage(image_3d)
     return img_data
 
-def create_working_folder_and_convert_to_nifti(patient_name, working_folder, path_to_convCT_folder, path_to_VMI40_folder):
+def create_working_folders_and_convert_to_nifti(patient_name, working_folder, working_folder_conv_CT, working_folder_conv_CT_in_RAS, working_folder_VMI40, working_folder_VMI40_cropped, working_folder_Segmentation, path_to_convCT_folder, path_to_VMI40_folder):
     maybe_mkdir_p(working_folder) 
+    maybe_mkdir_p(working_folder_conv_CT) 
+    maybe_mkdir_p(working_folder_conv_CT_in_RAS) 
+    maybe_mkdir_p(working_folder_VMI40) 
+    maybe_mkdir_p(working_folder_VMI40_cropped) 
+    maybe_mkdir_p(working_folder_Segmentation) 
 
     # Vytvoření čtečky DICOM série
     reader = sitk.ImageSeriesReader()    
@@ -80,9 +91,13 @@ def create_working_folder_and_convert_to_nifti(patient_name, working_folder, pat
     # Načtení 3D obrazu
     image = reader.Execute()    
     # Cesta a jméno výsledného NIfTI souboru
-    output_nifti_path = join(working_folder, patient_name + '_conv_0000.nii.gz')    
+    output_nifti_path = join(working_folder_conv_CT, patient_name + '_conv_0000.nii.gz')    
     # Uložení do NIfTI
     sitk.WriteImage(image, output_nifti_path)
+    
+    shutil.copy(output_nifti_path, join(working_folder_conv_CT_in_RAS, patient_name + '_conv_RAS_0000.nii.gz'))
+    reorient_all_images_in_folder_to_ras(working_folder_conv_CT_in_RAS,1)
+
 
     # Vytvoření čtečky DICOM série
     reader = sitk.ImageSeriesReader()    
@@ -92,6 +107,62 @@ def create_working_folder_and_convert_to_nifti(patient_name, working_folder, pat
     # Načtení 3D obrazu
     image = reader.Execute()    
     # Cesta a jméno výsledného NIfTI souboru
-    output_nifti_path = join(working_folder, patient_name + '_monoe_40kev_0000.nii.gz')    
+    output_nifti_path = join(working_folder_VMI40, patient_name + '_monoe_40kev_0000.nii.gz')    
     # Uložení do NIfTI
     sitk.WriteImage(image, output_nifti_path)
+
+
+    
+
+
+
+def reorient_all_images_in_folder_to_ras(folder: str, num_processes: int = 2):
+    p = Pool(num_processes)
+    nii_files = subfiles(folder, suffix='.nii.gz', join=True)
+    p.map(reorient_to_ras, nii_files)
+    p.close()
+    p.join()
+    
+def revert_orientation_on_all_images_in_folder(folder: str, num_processes: int = 1):
+    p = Pool(num_processes)
+    nii_files = subfiles(folder, suffix='.nii.gz', join=True)
+    p.map(revert_reorientation, nii_files)
+    p.close()
+    p.join()
+
+def reorient_to_ras(image: str) -> None:
+    """
+    Will overwrite image!!!
+    :param image:
+    :return:
+    """
+    assert image.endswith('.nii.gz')
+    origaffine_pkl = image[:-7] + '_originalAffine.pkl'
+    if not isfile(origaffine_pkl):
+        img = nib.load(image)
+        original_affine = img.affine
+        original_axcode = nib.aff2axcodes(img.affine)
+        img = img.as_reoriented(io_orientation(img.affine))
+        new_axcode = nib.aff2axcodes(img.affine)
+        print(image.split('/')[-1], 'original axcode', original_axcode, 'now (should be ras)', new_axcode)
+        nib.save(img, image)
+        save_pickle((original_affine, original_axcode), origaffine_pkl)
+
+
+def revert_reorientation(image: str) -> None:
+    assert image.endswith('.nii.gz')
+    expected_pkl = image[:-7] + '_originalAffine.pkl'
+    assert isfile(expected_pkl), 'Must have a file with the original affine, as created by ' \
+                                 'reorient_to_ras. Expected filename: %s' % \
+                                 expected_pkl
+    original_affine, original_axcode = load_pickle(image[:-7] + '_originalAffine.pkl')
+    img = nib.load(image)
+    before_revert = nib.aff2axcodes(img.affine)
+    img = img.as_reoriented(io_orientation(original_affine))
+    after_revert = nib.aff2axcodes(img.affine)
+    print('before revert', before_revert, 'after revert', after_revert)
+    restored_affine = img.affine
+    assert np.all(np.isclose(original_affine, restored_affine)), 'restored affine does not match original affine, ' \
+                                                                 'aborting!'
+    nib.save(img, image)
+    os.remove(expected_pkl)
--- run_prediction_of_nnUNet_networks.py
+++ run_prediction_of_nnUNet_networks.py
@@ -6,9 +6,9 @@ Created on Wed Aug 13 10:30:33 2025
 """
 import sys
 import os
-sys.path.append(os.path.abspath('F:\Spinal-Multiple-Myeloma-SEG\nnUNet'))
+sys.path.append(os.path.abspath('F:/Spinal-Multiple-Myeloma-SEG/nnUNet'))
 from nnunetv2.paths import nnUNet_results, nnUNet_raw, nnUNet_preprocessed
-from functions import create_working_folder_and_convert_to_nifti, run_nnunet_inference
+from functions import create_working_folders_and_convert_to_nifti, run_nnunet_inference
 import shutil
 import os
 join=os.path.join
@@ -49,7 +49,29 @@ if __name__ == "__main__":
 
     name_of_output_folder = patient_name + "_output"
     working_folder = join(path_to_output_folder,name_of_output_folder) 
-    create_working_folder_and_convert_to_nifti(patient_name, working_folder, path_to_convCT_folder, path_to_VMI40_folder)
+    working_folder_conv_CT = join(working_folder,'Conv_CT')
+    working_folder_conv_CT_in_RAS = join(working_folder,'Conv_CT_in_RAS')
+    working_folder_VMI40 = join(working_folder,'VMI40')
+    working_folder_VMI40_cropped = join(working_folder,'VMI40_cropped')
+    working_folder_Segmentation = join(working_folder,'Segmentation') 
+    create_working_folders_and_convert_to_nifti(patient_name, working_folder, working_folder_conv_CT, working_folder_conv_CT_in_RAS, working_folder_VMI40, working_folder_VMI40_cropped, working_folder_Segmentation, path_to_convCT_folder, path_to_VMI40_folder)
+
+    
+
+        # %% Segmentation of spine with nnUNet        
+    input_folder = working_folder_conv_CT_in_RAS 
+    output_folder = working_folder_Segmentation
+
+    dataset_name = "Dataset800_Spine_segmentation_trained_on_VerSe20"
+    trainer_name = "nnUNetTrainer__nnUNetPlans__3d_fullres"
+    use_folds = ('all',)
+
+    nnUNet_results = "F:/Spinal-Multiple-Myeloma-SEG_nnUNet_models"
+
+    run_nnunet_inference(nnUNet_results, dataset_name, trainer_name, use_folds, input_folder, output_folder) 
+
+
+
 
 
 
